35480491e333ef45f5c8a6763094edb57376e4ef
Toto <tomasprietodfrancia14@gmail.com>
2023-05-31 16:33:05 -0300

    Solo se analiza

--- Conceptos de Arquitectura/Practica 5/6.s
+++ Conceptos de Arquitectura/Practica 5/6.s
@@ -12,12 +12,14 @@ sd $v0, result($0)
 halt
 
 
-a_la_potencia: daddi $v0, $0, 1
+a_la_potencia: daddi $v0, $0, 1; eleva 16 a la 4ta
 
-lazo: slt $t1, $a1, $0
+lazo: slt $t1, $a1, $0;cuando se haya elevado hasta el valor2, termina 
 bnez $t1, terminar
 daddi $a1, $a1, -1
 dmul $v0, $v0, $a0
 j lazo
 
 terminar: jr $ra
+
+;d) Se usaria la pila para guardar las direcciones de varias subrutinas
