e160130108ccf5d20d8c69cc2f7537499f056a94
Toto <tomasprietodfrancia14@gmail.com>
2023-06-04 11:24:46 -0300

    punto 7 terminado

--- Programacion III/Practica 6/PruebaAlgoritmos.java
+++ Programacion III/Practica 6/PruebaAlgoritmos.java
@@ -26,25 +26,25 @@ public class PruebaAlgoritmos {
     ciudades.conectar(v6, v5, 5);
     ciudades.conectar(v7, v5, 5);
 
-    ciudades.conectar(v5, v3, 0);//para que sea cuadrado en Lima si lo descomento
-    //ciudades.conectar(v7, v1, 5);//pa que sea cuadrado en bsas si lo descomento
-    //ciudades.conectar(v3, v2, 3);//pa que sea cuadrado en Santiago si lo descomento (necesita de v5 a v3)
-    //ciudades.conectar(v3, v1, 0);//pa que puedan haber mas ciclos no cuadrados
+    ciudades.conectar(v5, v3, 0);//cuadrado en Lima si lo descomento
+    ciudades.conectar(v7, v1, 5);//cuadrado en bsas si lo descomento
+    ciudades.conectar(v3, v1, 0);//ciclos no cuadrados
 
-    //PA DEBUGUEAR EJ 7A CUADRADO
-      Algoritmos<String> a = new Algoritmos<String>();
+    //Tiene cuadrado
+    /*   Algoritmos<String> a = new Algoritmos<String>();
      if(a.subgrafoCuadrado(ciudades)==true)
          System.out.println("Grafo cuadrado\n");
      else
          System.out.println("Grafo NO cuadrado\n");
+         */
 
-  //  pa debuguar 7b grado
-  /*  Algoritmos<String> a = new Algoritmos<String>();
+  // Si tiene ciclo
+    Algoritmos<String> a = new Algoritmos<String>();
     if(a.tieneCiclo(ciudades)==true)
       System.out.println("Tiene al menos un ciclo \n");
     else
       System.out.println("No tiene ciclos \n");
-      */
+      
    
 }
 }
