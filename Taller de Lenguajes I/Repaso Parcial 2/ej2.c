f98393aca26b0a6f604ab1607847c7d7af6f495b
Toto <tomasprietodfrancia14@gmail.com>
2023-06-08 21:03:11 -0300

    Primer piramide. Eficiente. Libera

--- Taller de Lenguajes I/Repaso Parcial 2/ej2.c
+++ Taller de Lenguajes I/Repaso Parcial 2/ej2.c
@@ -13,18 +13,18 @@ elementos de base.
 typedef int ** Piramide;
 
 void liberar_piramide(Piramide p, int n);
-void Leer(Piramide matriz, int F, int C);
+void Leer(Piramide matriz, int F);
 Piramide crear_piramide(int);
 
 int main(){
 
     Piramide matriz=crear_piramide(NUM);
 
-    Leer(matriz, NUM, NUM);
+    Leer(matriz, NUM);
 
     liberar_piramide(matriz, NUM);
 
-    Leer(matriz, NUM, NUM);
+    Leer(matriz, NUM);
 }
 
 
@@ -34,13 +34,13 @@ int main(){
 Piramide crear_piramide(int n){
     Piramide matriz;
     matriz=(int**)calloc(n, sizeof(int*));
-    int i,j,k=0;
+    int i,j,k=1;
     for (i=0; i<n; i++){
-        matriz[i]=(int*)calloc(n,sizeof(int));
+        matriz[i]=(int*)calloc((i)+1,sizeof(int));
     }
     srand(time(NULL));
     for(i=0;i<n;i++){
-        for(j=0;j<=k;j++){
+        for(j=0;j<k;j++){
             matriz[i][j]=1+rand()%8;
         }
         k++;
@@ -50,15 +50,14 @@ Piramide crear_piramide(int n){
 }
 
 
-void Leer(Piramide matriz, int F, int C){
-     int i,j;
+void Leer(Piramide matriz, int F){
+     int i,j,k=1;
      for(i=0;i<F;i++){
-        for(j=0;j<C;j++){
-           if(matriz[i][j]!=0){
+        for(j=0;j<k;j++){
             printf("%d  ", matriz[i][j]);
-           }
         }
         printf("\n");
+        k++;
     }
 }
 
